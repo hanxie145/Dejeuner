8496a395dd1e69af27acb458776af06cf8addfeb
TonyXie <txie145@gmail.com>
2013-08-06 17:22:34 +0200

    url to image-url

--- app/models/number.rb
+++ app/models/number.rb
@@ -1,2 +1,3 @@
 class Number < ActiveRecord::Base
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130806112152) do
+ActiveRecord::Schema.define(version: 20130806151129) do
+
+  create_table "numbers", force: true do |t|
+    t.integer  "number"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.string   "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
