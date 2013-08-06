adb98893bf23cebac783a2d6564b86a8351e1679
TonyXie <txie145@gmail.com>
2013-08-06 17:50:54 +0200

    added numbers by sms

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,11 +3,13 @@ class TwilioController < ApplicationController
   def sms_response
     # respond to a text message from twilio
     review = params[:Body]
+    number = params[:From]
 
     # hacccckkkkyyy
     tony = User.find_by_email('txie145@gmail.com')[0]
 
     tony.reviews.create(body: review)
+    tony.numbers.create(number: number )
 
     render 'sms-response.xml.erb', :content_type => 'text/xml'
   end 
