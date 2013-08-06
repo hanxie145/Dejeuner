852bc3ab33e9beb6439f0093dcf814f8e3094c5e
TonyXie <txie145@gmail.com>
2013-08-06 11:07:15 +0200

    initial

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'faker', '~> 1.0.1'
+gem 'guard-rspec', '0.5.5'
+gem 'thin'
+gem 'haml-rails'
+gem 'devise'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+gem 'twilio-ruby'
+
+group :development, :test do
+  gem "better_errors"
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'rails_12factor'
+  gem 'pg' 
+end
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dejeuner::Application.config.secret_key_base = 'e3c360a77ad95eb3769ec38700b14607e33c9f3d4dc94eeb102c2cc64e7af3fafdbe669bbfc5c88d7f24e701d1f1bd11d85b9643b98f0bc90f8c622c0a5cecc2'
