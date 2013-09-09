b0893364af7e5e5ea00cf6c71caaefb013f2fef2
TonyXie <txie145@gmail.com>
2013-09-09 22:52:35 +0200

    i

--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -1,17 +1,28 @@
 require 'spec_helper'
 
 describe ProductController do 
+
+  # sign in user and by pass authentication
   before {@user = create(:user)}
+  before { controller.stub(:authenticate_user!).and_return true } 
+  before {sign_in @user}
 
   describe 'GET #main page' do 
-    before { controller.stub(:authenticate_user!).and_return true } 
-    before {sign_in @user}
 
     it "#main should be valid" do 
       get :main
       response.should be_success
     end
 
+  end
+
+  describe 'GET #profile' do
+
+    it "#profile should be valid" do 
+      get :profile 
+      response.should be_success
+    end
+    
   end
 
 end
