fae322eff0466164c4c261bc3e4dc3096a09d1a8
TonyXie <txie145@gmail.com>
2013-11-29 22:58:09 -0800

    removed campaign type from campaigns

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,7 +1,6 @@
 class Campaign < ActiveRecord::Base
   belongs_to :user
 
-  validates :campaign_type, presence: true
   validates :description, presence: true
   validates :description, length: {maximum: 400}
 
