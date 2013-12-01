a154d9d1ce3a5ac0c307b6bd1338bc02653870d4
TonyXie <txie145@gmail.com>
2013-12-01 10:18:13 -0800

    added additional parameters for user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :configure_permitted_parameters, if: :devise_controller?
   include ActionView::Helpers::TextHelper, ApplicationHelper
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   
   # for devise routing
   def after_sign_in_path_for(resource)
@@ -16,18 +16,8 @@ class ApplicationController < ActionController::Base
 
   protected 
 
-  # add form attributes for devise sign ups
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :name, :restaurant, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
   end
-
-  # for devise edit 
-   def devise_parameter_sanitizer
-    if resource_class == User
-      User::ParameterSanitizer.new(User, :user, params)
-    else
-      super
-    end
-  end 
-  
+    
 end
