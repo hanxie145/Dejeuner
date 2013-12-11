06e1d012db3674bf3c4cba135f02514dc4b7b377
TonyXie <txie145@gmail.com>
2013-12-10 22:19:47 -0800

    changed time zone code for scheduling

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -17,7 +17,7 @@ class SmsController < ApplicationController
       year = params[:year]
       month = params[:month]
       date = params[:date]
-      time_to_send_at = Time.new(year, month, date, hour, minute, second)
+      time_to_send_at = Time.zone.local(year, month, date, hour, minute, second)
 
       # Send the message in the background at the time specified
       current_user.delay(run_at: time_to_send_at).send_sms_message params[:message]
