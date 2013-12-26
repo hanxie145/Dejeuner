c8ca79023fface3173c4d49c14bb40c1b8c1c495
TonyXie <txie145@gmail.com>
2013-12-25 21:46:32 -0800

    scheduling for messages

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -10,22 +10,29 @@ class SmsController < ApplicationController
       flash[:notice] = "You do not have enough sms credits remaining! Please refill your sms credits."
       redirect_to market_path
     else
-      # get the time and create a new dateTime object
-      params[:meridian] === 'PM' ? hour = params[:hour].to_i + 12 : hour = params[:hour].to_i
-      minute = params[:minute].to_i
-      second = params[:second].to_i
-      year = params[:year]
-      month = params[:month]
-      date = params[:date]
+      # figure out if the user wants to the send the message now or later. 
+      if params[:message_now_or_later] === 'now'
+        @user.delay.send_sms_message params[:message]
+        flash[:notice] = "Message send successful"
+        redirect_to main_path
+      elsif params[:message_now_or_later] === 'later'
+        # get the time and create a new dateTime object
+        params[:meridian] === 'PM' ? hour = params[:hour].to_i + 12 : hour = params[:hour].to_i
+        minute = params[:minute].to_i
+        second = params[:second].to_i
+        year = params[:year]
+        month = params[:month]
+        date = params[:date]
 
-      # schedule the message according to the user's time zone. Create the message time in the user's time zone then convert to UTC +0000 (the application's time zone)
-      Time.zone = @user.time_zone
-      time_to_send_at = Time.zone.local(year, month, date, hour, minute, second).in_time_zone('UTC')
+        # schedule the message according to the user's time zone. Create the message time in the user's time zone then convert to UTC +0000 (the application's time zone)
+        Time.zone = @user.time_zone
+        time_to_send_at = Time.zone.local(year, month, date, hour, minute, second).in_time_zone('UTC')
 
-      # Send the message in the background at the time specified
-      @user.delay(run_at: time_to_send_at, user_id: @user.id).send_sms_message params[:message]
-      flash[:notice] = "Message send successful"
-      redirect_to main_path
+        # Send the message in the background at the time specified
+        @user.delay(run_at: time_to_send_at, user_id: @user.id).send_sms_message params[:message]
+        flash[:notice] = "Message successfully scheduled."
+        redirect_to '/scheduled_messages'
+      end
     end
   end
 end
